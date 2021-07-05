57b803135023e8ad576058afb9e90bff91660d30
gh_fuyingfei <fuyingfei@ghgame.cn>
2021-07-05 17:09:19 +0800

    加个本地调试服务器

---
+++ server/http.js
@@ -0,0 +1,53 @@
+var PORT = 80;//
+
+var http = require('http');
+var url=require('url');
+var fs=require('fs');
+var mine=require('./mine').types;//
+var path=require('path');
+var webapp = '../';
+var server = http.createServer(function (request, response) {
+	console.log('url', request.url);
+    var pathname = url.parse(request.url).pathname;
+	console.log('pathname', pathname);
+    var realPath = path.join(webapp, pathname);    //这里设置自己的文件名称;
+	var ext = path.extname(realPath);
+	
+	if(pathname === '/' && ext === '')
+	{
+		pathname = '/index.html';
+		
+	}
+    realPath = path.join(webapp, pathname);
+	ext = path.extname(realPath);
+	
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end();
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+					
+                    response.end();
+                }
+            });
+        }
+    });
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
